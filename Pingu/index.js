b980a74e479185e20d46541bc8f4abee2ce0e4e1
Ole Henrik Hellenes <oh.hellenes@gmail.com>
2017-11-03 20:16:26 +0100

    add example post payload json

--- Pingu/index.js
+++ Pingu/index.js
@@ -21,29 +21,31 @@ function info(){
 function action (body){
 
     // Get battle-state of pingu the unbroken
-    /*matchOpts = {
-        matchId: body.matchId, // unik kamp-ID 
-        mapWidth: body.mapWidth,
-        mapHeight: body.mapHeight,
-        suddenDeath: body.suddenDeath, // antall runder til sudden death starter
-        wallDamage: body.wallDamage, // hvor mye skade som pingvinen tar av å gå inn i veggen
-        penguinDamage: body.penguinDamage, // hvor mye skade pingvinen tar av å krasje med en annen pingvin -- også hvor mye skade pingvinen din gjør på veggen når den krasjer med den
-        weaponDamage: body.weaponDamage, // hvor mye skade pingvinen tar av å bli skutt
-        visibility: body.visibility, // avstanden i felt du kan se på radaren
-        weaponRange: body.weaponRange, // avstranden i felt pingvinens laser rekker
-        you: body.PENGUIN, // informasjon om din pingvin -- se pingvinstrukturen nedenfor
-        enemies: body.enemies, // array. fiendlige pingviner. se struktur nedenfor
-        bonusTiles: body.bonusTiles, //array. felter som inneholder bonuser. se bonusstruktur nedenfor
-        walls: body.walls, // array. synlige vegger. se vegg-struktur nedenfor
-        fire: body.fire, // array, synlige felt som brenner
-        command: doSomethingRandom
-    };*/
+    if (body) {
+        matchOpts = {
+            matchId: body.matchId, // unik kamp-ID 
+            mapWidth: body.mapWidth,
+            mapHeight: body.mapHeight,
+            suddenDeath: body.suddenDeath, // antall runder til sudden death starter
+            wallDamage: body.wallDamage, // hvor mye skade som pingvinen tar av å gå inn i veggen
+            penguinDamage: body.penguinDamage, // hvor mye skade pingvinen tar av å krasje med en annen pingvin -- også hvor mye skade pingvinen din gjør på veggen når den krasjer med den
+            weaponDamage: body.weaponDamage, // hvor mye skade pingvinen tar av å bli skutt
+            visibility: body.visibility, // avstanden i felt du kan se på radaren
+            weaponRange: body.weaponRange, // avstranden i felt pingvinens laser rekker
+            you: body.PENGUIN, // informasjon om din pingvin -- se pingvinstrukturen nedenfor
+            enemies: body.enemies, // array. fiendlige pingviner. se struktur nedenfor
+            bonusTiles: body.bonusTiles, //array. felter som inneholder bonuser. se bonusstruktur nedenfor
+            walls: body.walls, // array. synlige vegger. se vegg-struktur nedenfor
+            fire: body.fire, // array, synlige felt som brenner
+            command: doSomethingRandom
+        };
 
-    return req;
+        return matchOpts;
+    }
 
-    /*return {
-            command: doSomethingRandom()
-            };*/
+    return {
+        command: doSomethingRandom()
+    };
 }
 
 function getBody(req){
@@ -51,7 +53,7 @@ function getBody(req){
         case 'GET':
             return info();
         case 'POST':
-            return action(req);
+            return action(req.body);
     }
 }
 
