9ba881f6250e04ee199d237976f7ff33e0780d49
Ole Henrik Hellenes <oh.hellenes@gmail.com>
2017-11-04 00:55:48 +0100

    More indentation fix. Copy/paste krekle-pingu to Pingu

--- Pingu/index.js
+++ Pingu/index.js
@@ -1,59 +1,29 @@
-// Globals
-/*var commands = {
-    rotateright: "rotate-right",
-    rotateleft: "rotate-left",
-    advance: "advance",
-    retreat: "retreat",
-    shoot: "shoot"
-};*/
+var nextAction;
 
-function doSomethingRandom(){
-    var _commands = [
-        "rotate-right",
-        "rotate-left",
-        "advance",
-        "retreat",
-        "shoot"
-    ];
-    var rnd = Math.floor(Math.random() * 5);
-
-    return _commands[rnd];
+// Agressive
+function distanceToEnemy(currentPosition, enemyPosition) {
+    var distanceX = Math.abs(currentPosition.x - enemyPosition.x);
+    var distanceY = Math.abs(currentPosition.y - enemyPosition.y);
+    var distance = Math.sqrt(distanceX*distanceX + distanceY*distanceY);
+    return distance;
 }
 
-function info(){
-    return {
-        name: "Mr. NootNoot",
-        team: "Nooting but net!"
-    }
-}
-
-function calculateAction(opts) {
-    var pingu = opts.you;
-
-    var shouldShoot = shouldShoot(pingu, opts.enemies);
-    if (shouldShoot) {
-        return 'shoot';
-    } else {
-        return doSomethingRandom();
-    }
-}
-
-function shouldShoot(currentPosition, enemyPositions) {
+function shouldShot(currentPosition, enemyPositions) {
     enemyPositions.forEach(function(enemy) {
         if (distanceToEnemy(currentPosition, enemy) < 3) {
-            shoot = false
+            var shoot = false;
             switch (currentPosition.direction) {
                 case "top":
-                shoot = enemy.x == currentPosition.x && enemy.y > currentPosition.y
+                shoot = enemy.x == currentPosition.x && enemy.y > currentPosition.y;
                 break;
                 case "bottom":
-                shoot = enemy.x == currentPosition.x && enemy.y < currentPosition.y
+                shoot = enemy.x == currentPosition.x && enemy.y < currentPosition.y;
                 break;
                 case "left":
-                shoot = enemy.y == currentPosition.y && enemy.x < currentPosition.x
+                shoot = enemy.y == currentPosition.y && enemy.x < currentPosition.x;
                 break;
                 case "right":
-                shoot = enemy.y == currentPosition.y && enemy.x > currentPosition.x
+                shoot = enemy.y == currentPosition.y && enemy.x > currentPosition.x;
                 break;
             }  
             if (shoot) {
@@ -65,56 +35,50 @@ function shouldShoot(currentPosition, enemyPositions) {
     return false;
 }
 
-// Utilities
-/*function findClosestEnemy(you, enemies, fieldOfViewRadius) {
-    var closestEnemy = null;
-    enemies.forEach(function(enemy) {
-        enemyIsInRange = (enemy.x < you.x + fieldOfViewRadius) && (enemy.y < you.y + fieldOfViewRadius);
+// Survival
 
-        if (enemyIsInRange) {
-            // Is enemy closer than the previous?
-            if ((enemy.x + enemy.y) < (closestEnemy.x + closestEnemy.y)) {
-                closestEnemy.x = enemy.x;
-                closestEnemy.y = enemy.y;
-            }
+
+// Movement
+
+
+function calculateMove(state){
+    // Controls
+    var direction = state.you.direction;
+    if(nextAction) {
+        if(nextAction != direction) {
+            return "rotate-right";
         }
-    }, this);
 
-    return closestEnemy;
-}*/
+        return "advance";
+    }
+    
+    var commands = [
+        "rotate-right",
+        "rotate-left",
+        "advance",
+        "retreat",
+        "shoot"
+    ];
 
-function action (body){
-
-    // Get battle-state of pingu the unbroken
-    if (body) {
-        body = JSON.parse(body);
-        /*var matchOpts = {
-            matchId: body.matchId, // unik kamp-ID 
-            mapWidth: body.mapWidth,
-            mapHeight: body.mapHeight,
-            suddenDeath: body.suddenDeath, // antall runder til sudden death starter
-            wallDamage: body.wallDamage, // hvor mye skade som pingvinen tar av å gå inn i veggen
-            penguinDamage: body.penguinDamage, // hvor mye skade pingvinen tar av å krasje med en annen pingvin -- også hvor mye skade pingvinen din gjør på veggen når den krasjer med den
-            weaponDamage: body.weaponDamage, // hvor mye skade pingvinen tar av å bli skutt
-            visibility: body.visibility, // avstanden i felt du kan se på radaren
-            weaponRange: body.weaponRange, // avstranden i felt pingvinens laser rekker
-            you: body.you, // informasjon om din pingvin -- se pingvinstrukturen nedenfor
-            enemies: body.enemies, // array. fiendlige pingviner. se struktur nedenfor
-            bonusTiles: body.bonusTiles, //array. felter som inneholder bonuser. se bonusstruktur nedenfor
-            walls: body.walls, // array. synlige vegger. se vegg-struktur nedenfor
-            fire: body.fire, // array, synlige felt som brenner
-            command: doSomethingRandom()
-        };*/
-
-        var action = calculateAction(body);
-
-        return {
-            command: action
-        };
+    if (shouldShot(state.you, state.enemies)) {
+        return commands[4];
     }
 
+    // Fallback
+    var rnd = Math.floor(Math.random() * 5);
+    return commands[rnd];
+}
+
+function info(){
     return {
-        command: doSomethingRandom()
+        name: "Joda",
+        team: ":)"
+    }
+}
+
+function action(state) {
+    return {
+        command: calculateMove(state)
     };
 }
 
@@ -124,6 +88,10 @@ function getBody(req){
             return info();
         case 'POST':
             return action(req.body);
+        case 'PATCH':
+            const previousAction = nextAction;
+            nextAction = req.body.nextAction;
+            return { nextAction, previousAction };
     }
 }
 
