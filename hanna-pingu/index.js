8655d62c01b74b4bcf1f911255e657b1839f8af9
Hanna Sollie <hannss@stud.ntnu.no>
2017-11-04 00:53:20 +0100

    copy-pasting

--- hanna-pingu/index.js
+++ hanna-pingu/index.js
@@ -1,6 +1,25 @@
-function moveRandom(){
-    var commands = ["rotate-right", "rotate-left", "advance", "retreat"];
-    var rnd = Math.floor(Math.random() * 4);
+var nextAction;
+
+function doSomethingRandom(state){
+    var direction = state.you.direction;
+
+    if(nextAction) {
+        if(nextAction != direction) {
+            return "rotate-right";
+        }
+
+        return "advance";
+    }
+
+    var commands = [
+        "rotate-right",
+        "rotate-left",
+        "advance",
+        "retreat",
+        "shoot"
+    ];
+    var rnd = Math.floor(Math.random() * 5);
+
     return commands[rnd];
 }
 
@@ -11,19 +30,22 @@ function info(){
     }
 }
 
-function action(body) {
-  return {
-    command: moveRandom()
-  };
+function action(state) {
+    return {
+        command: doSomethingRandom(state)
+    };
 }
-
 
 function getBody(req){
     switch(req.method){
         case 'GET':
             return info();
         case 'POST':
-            return action(req);
+            return action(req.body);
+        case 'PATCH':
+            const previousAction = nextAction;
+            nextAction = req.body.nextAction;
+            return { nextAction, previousAction };
     }
 }
 
@@ -31,9 +53,26 @@ module.exports = function (context, req) {
 
     context.log('JavaScript HTTP trigger function processed a request.', req.body);
     context.res = {
-        headers: {
-            "Access-Control-Allow-Origin": "*"  
-        },
+        body: getBody(req)
+    };
+    context.done();
+};function getBody(req){
+    switch(req.method){
+        case 'GET':
+            return info();
+        case 'POST':
+            return action(req.body);
+        case 'PATCH':
+            const previousAction = nextAction;
+            nextAction = req.body.nextAction;
+            return { nextAction, previousAction };
+    }
+}
+
+module.exports = function (context, req) {
+
+    context.log('JavaScript HTTP trigger function processed a request.', req.body);
+    context.res = {
         body: getBody(req)
     };
     context.done();
